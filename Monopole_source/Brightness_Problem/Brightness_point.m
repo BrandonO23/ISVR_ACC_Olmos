9b6beb08fccc194ab706a679c61d02421ca896be
BrandonO <brandonolmos23@gmail.com>
2018-03-11 20:55:06 +0000

    Add-Brightness control
    Though it is non-working at the moment. Also need to add array effort contraints.

--- Monopole_source/Brightness_Problem/Brightness_point.m
+++ Monopole_source/Brightness_Problem/Brightness_point.m
@@ -1,31 +1,32 @@
 %% Brightness Problem
 clc
 clear
+it = 1;
 
-for n = -.5:.01:.5
+for f = 100:100:10000
 
 
-f = 1000;            % Single frequency
+% f = 1000;            % Single frequency
 omega = 2*pi*f;      % Angular frequency 
 c = 344;             % Speed of sound
 lambda = c./f;       % Wavelength
 rho = 1.225;         % Density of air
 k = 2*pi./lambda;    % Wave number
-Jo = .00000005;          % square modulate volume Velocity Contraint
+Jo = (.8*5e-5)^2;      % square modulus volume Velocity Contraint
 
 delta = .01;
-rx = -.5:delta:.5;                 % x
-ry = 0:delta:.5;                  % y
+rx = -.8:delta:.8;                 % x
+ry = 0:delta:1;                  % y
 [X, Y] = meshgrid(rx,ry);        % Meshgrid from rx and ry
-b = [n,.2];
+b = [.3,.3];
 row = find(abs(10000000000*(rx-b(1)))<1);
 col = find(abs(10000000000*(ry-b(2)))<1);
 
 % Source positions in meters
-Cs = [.1 0;
-      -.1 0;
-      .02 0;
-      -.02 0];  
+Cs = [.005 0;
+      -.005 0;
+      .02 -.01;
+      -.02 -.01];  
   
 l = size(Cs,1);                  % Amount of Control Sources
 MeshZ = cell(l,1);
@@ -44,15 +45,17 @@ for i = 1:l
     p = p + green{i}.*q(i);
 end
 
-surf(rx,ry,(real(p)),'edgecolor', 'none')
+surf(rx,ry,(abs(p)),'edgecolor', 'none')
 colormap('jet')
-caxis([-1 1])
+caxis([0 1])
 view(0,90)
 colorbar
 xlabel('Meters'),ylabel('Meters')
 hold on
 scatter3(b(1),b(2),10,'o','linewidth',2,'MarkerFaceColor','k','MarkerEdgeColor','k')
 hold off
+q1(:,it) = q;
+it = it+1;
 pause(.1)
 
 end
